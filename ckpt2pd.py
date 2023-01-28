3de024f43c69b16c8645f2b8a616f8d28d48a0e9
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-01-28 14:24:52 +0900

    add

---
+++ ckpt2pd.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+graph = tf.get_default_graph()
+
+sess = tf.Session()
+
+saver = tf.train.import_meta_graph('./trained_models/tshirt/garment_fit/model.meta')
+saver.restore(sess, 'model.data-00000-of-00001')
+
+tf.train.write_graph(sess.graph_def, '.', 'graph.pb', as_text=False)
