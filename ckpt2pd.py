36769ffd2024c15d47f434c6c015ed45032eb198
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-01-28 14:36:02 +0900

    add

--- ckpt2pd.py
+++ ckpt2pd.py
@@ -6,6 +6,6 @@ graph = tf.get_default_graph()
 sess = tf.Session()
 
 saver = tf.train.import_meta_graph('./trained_models/tshirt/garment_fit/model.meta')
-saver.restore(sess, './trained_models/tshirt/garment_fit/checkpoint')
+saver.restore(sess, './trained_models/tshirt/garment_fit/model')
 
 tf.train.write_graph(sess.graph_def, '.', 'graph.pb', as_text=False)
